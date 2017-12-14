2d7e44c0c478f89f214c7b02f53aa335cc0138f5
kkamburov <Kaloyan.Kamburov@experian.com>
2017-12-14 17:27:52 +0200

    pushec

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,19 @@
+class TimersDashboard extends React.Component {
+    render() {
+        return(
+            <div className='ui three column centered grid'>
+                <div className='column'>sss
+                    <EditableTimerList />
+                    <ToggeableTimerForm
+                        isOpen={true}
+                    />
+                </div>
+            </div>
+        );
+    }
+}
 
+ReactDOM.render(
+    <TimersDashboard />,
+    document.getElementById('content')
+);
